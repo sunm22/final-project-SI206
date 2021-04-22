e1301bea67fe83574643805b8f45376f299d5971
mxsun <mxsun@umich.edu>
2021-04-22 14:13:02 -0400

    added function descriptions

--- minnie.py
+++ minnie.py
@@ -34,13 +34,15 @@ def percent_change_table(cur, conn, state, percent):
     #STATE MUST BE LOWERCASE
     '''This function takes in cursor and connection variables to database, state,
     and percent change calculated from percent_change. It creates a table in the database
-    if it doesn't exist and inserts the state, and percent change. Returns nothing.'''
+    if it doesn't exist and inserts the state and percent change. Returns nothing.'''
 
     cur.execute('CREATE TABLE IF NOT EXISTS PercentChange ("state" TEXT, "percent_change" NUMBER)')
     cur.execute('INSERT INTO PercentChange (state, percent_change) VALUES (?, ?)', (state, percent))
     conn.commit()
 
 def covid_table_length(cur, conn):
+    '''This function calculates the number of rows in the CovidData table to help with extracting
+    25 lines at a time. Returns the number of rows in the table as an int.'''
     cur.execute('CREATE TABLE IF NOT EXISTS CovidData ("id" INTEGER PRIMARY KEY, "state" TEXT, "date" NUMBER, "number_of_cases" NUMBER)')
     cur.execute('SELECT id FROM CovidData')
     data = cur.fetchall()
@@ -51,9 +53,9 @@ def covid_table_length(cur, conn):
 
 ##################################################################
 
-def get_2021_data(cur, conn, state):
+def get_mar_data(cur, conn, state):
     '''This function takes in the cursor and connection variables, and the lowercase state abbreviation.
-    It sends requests to COVID Tracking Project API for the latest data for the given state (usually March 7th 2021, as that's when
+    It sends requests to COVID Tracking Project API for the latest data for the given state (mostly March 7th 2021, as that's when
     the project ended). Uses json to extract date and number of positive cases. Calls covid_table to create and add to table.
     Returns nothing.'''
     
@@ -71,47 +73,54 @@ def get_2021_data(cur, conn, state):
     #add to table
     covid_table(cur, conn, state, curr_date, curr_positive)
 
-def get_2020_data(cur, conn, state):
+def get_feb_data(cur, conn, state):
     '''This function takes in the cursor and connection variables, and the lowercase state abbreviation.
-    It sends requests to COVID Tracking Project API for March 7th, 2020 data for the given state.
+    It sends requests to COVID Tracking Project API for February 7th, 2021 data for the given state.
     Uses json to extract date and number of positive cases. Calls covid_table to create and add to table.
     Returns nothing.'''
-    url_2020 = f"https://api.covidtracking.com/v1/states/{state}/daily.json"
-    req = requests.get(url_2020)
+    url = f"https://api.covidtracking.com/v1/states/{state}/daily.json"
+    req = requests.get(url)
     curr_info = json.loads(req.text)
     curr_info.reverse()
 
-    feb_7_2020 = 20210207
+    feb_7_2021 = 20210207
     positive = 0
 
     #extract positive cases
     for day in curr_info:
-        if day["date"] == feb_7_2020:
+        if day["date"] == feb_7_2021:
             positive = day["positive"]
 
     if positive == 'null':
-        print("2020 info not found")
+        print("Feb info not found")
 
     #add to table
-    covid_table(cur, conn, state, feb_7_2020, positive)
+    covid_table(cur, conn, state, feb_7_2021, positive)
 
 def percent_change(cur, conn, state):
+    '''This function takes in cursor and connection variables, and the lowercase state abbreviation.
+    It calculates the percent change from Feb 2021 to Mar 2021 in number of COVID cases for the given state.
+    It adds the information to the PercentChange table by calling percent_change_table().
+    Returns nothing.'''
     cur.execute('SELECT number_of_cases FROM CovidData WHERE state=?', [state])
 
     row_count = 1
-    positive_2020 = 0
-    positive_2021 = 0
+    positive_feb = 0
+    positive_mar = 0
     for row in cur:
         if row_count == 1:
-            positive_2020 = row[0]
+            positive_feb = row[0]
         if row_count == 2:
-            positive_2021 = row[0]
+            positive_mar = row[0]
         row_count = row_count + 1
             
-    percent = (positive_2021 - positive_2020) / positive_2020 * 100
+    percent = (positive_mar - positive_feb) / positive_feb * 100
     percent_change_table(cur, conn, state, percent)
 
 def percent_change_viz(cur, conn):
+    '''This function takes in the cursor and connection variables. It uses matplotlib to create a bar graph
+    of the 10 states with highest % increase in COVID cases from Feb 2021 to Mar 2021 by using the PercentChange table.
+    Output is the creation of the graph.'''
     percent_list = []
     cur.execute('SELECT state, percent_change FROM PercentChange')
     for row in cur:
@@ -131,7 +140,7 @@ def percent_change_viz(cur, conn):
 
     plt.bar(x_pos, y, color='green')
     plt.xlabel('State')
-    plt.ylabel('Percent Change')
+    plt.ylabel('Percent Change (%)')
     plt.title('Highest percent changes in COVID cases from Feb 2021 to Mar 2021')
 
     plt.xticks(x_pos, x)
@@ -139,6 +148,9 @@ def percent_change_viz(cur, conn):
     plt.show()
 
 def highest_positives_viz(cur, conn):
+    '''This function takes in the cursor and connection variables. It uses matplotlib to create a bar graph
+    of the 10 states with highest # of COVID cases in Mar 2021 by using the CovidData table.
+    Output is the creation of the graph.'''
     positives_list = []
     cur.execute('SELECT state, number_of_cases FROM CovidData WHERE date = 20210307')
     for row in cur:
@@ -158,7 +170,7 @@ def highest_positives_viz(cur, conn):
 
     plt.bar(x_pos, y, color='blue')
     plt.xlabel('State')
-    plt.ylabel('Positive Cases on Mar 7, 2021')
+    plt.ylabel('Positive Cases on Mar 7, 2021 (millions)')
     plt.title('Highest positive cases by state in Mar 2021')
 
     plt.xticks(x_pos, x)
@@ -166,6 +178,10 @@ def highest_positives_viz(cur, conn):
     plt.show()
 
 def main():
+    '''Main includes two state abbreviation lists with 25 states in each. It calls covid_table_length() and
+    uses the results to determine what set of data to collect, storing 25 rows each time until CovidData is
+    populated with 100 rows. Then calcuates and populates PercentChange. Creates all visualizations.
+    Returns nothing.'''
     cur, conn = setUpDatabase("finalProject.db")
 
     states_list_1 = ['al', 'ak', 'az', 'ar', 'ca', 'co', 'ct', 'de', 'fl', 'ga', 'hi', 'id', 'il', 'in', 'ia', 'ks', 'ky', 'la', 'me', 'md', 'ma', 'mi', 'mn', 'ms', 'mo']
@@ -175,25 +191,25 @@ def main():
     if num == 0:
         print("1")
         for state in states_list_1:
-            get_2020_data(cur, conn, state)
+            get_feb_data(cur, conn, state)
         return
 
     elif num <= 25:
         print("2")
         for state in states_list_2:
-            get_2020_data(cur, conn, state)
+            get_feb_data(cur, conn, state)
         return
 
     elif num <= 50:
         print("3")
         for state in states_list_1:
-            get_2021_data(cur, conn, state)
+            get_mar_data(cur, conn, state)
         return
     
     elif num <= 75:
         print("4")
         for state in states_list_2:
-            get_2021_data(cur, conn, state)
+            get_mar_data(cur, conn, state)
     
     print("percent 1")
     for state in states_list_1:
